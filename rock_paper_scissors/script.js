14cd76d7223cebe6327f8dfe2ca06a482b846371
jaehrling <david-jaehrling@web.de>
2025-07-11 20:17:24 +0200

    Reimplement rock paper scissors

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -27,7 +27,35 @@ function playRound() {
 
   const result = determineWinner(playerChoice, computerChoice);
   console.log(result);
+  return result;
 }
 
-// Play the game
-playRound();
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 1; i <= 5; i++) {
+    console.log(`Round ${i}`);
+    const result = playRound();
+
+    if (result === "You win!") {
+      playerScore++;
+    } else if (result === "Computer wins!") {
+      computerScore++;
+    }
+
+    console.log(`Score after Round ${i}: Player - ${playerScore}, Computer - ${computerScore}`);
+  }
+
+  console.log("Game Over!");
+  if (playerScore > computerScore) {
+    console.log("You are the overall winner!");
+  } else if (computerScore > playerScore) {
+    console.log("Computer is the overall winner!");
+  } else {
+    console.log("It's an overall tie!");
+  }
+}
+
+// Start the game
+game();
